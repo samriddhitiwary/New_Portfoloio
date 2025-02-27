e63402048e7c293697f5b69b05fcb5d0710b39de
samriddhitiwary <151003533+samriddhitiwary@users.noreply.github.com>
2025-02-27 13:47:10 +0530

    Achievemnt Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 import Image from "next/image";
-import bg from "../../public/background/home-background.png";
 import RenderModel from "@/components/RenderModel";
-// import Wizard from "@/components/models/Wizard";
 import Navigation from "@/components/navigation";
-
 import dynamic from "next/dynamic";
+
 const Wizard = dynamic(() => import("@/components/models/Wizard"), {
   ssr: false,
 });
@@ -12,14 +10,16 @@ const Wizard = dynamic(() => import("@/components/models/Wizard"), {
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <Image
-        priority
-        sizes="100vw"
-        src={bg}
-        alt="background-image"
-        fill
-        className="-z-50 w-full h-full object-cover object-center opacity-50"
-      />
+      {/* Corrected Image Path */}
+      <div className="fixed top-0 left-0 w-full h-full -z-50">
+        <Image
+          src="/background/contact-background.png"
+          alt="Home background image"
+          priority
+          fill
+          className="object-cover object-center opacity-50"
+        />
+      </div>
 
       <div className="w-full h-screen">
         <Navigation />
